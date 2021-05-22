d84cb257bd5aed6ccf10dc5330e562fa170f80fd
Daniel Toba <danieloloruntoba681@gmail.com>
2021-05-22 16:11:35 +0100

    Modified

--- index.js
+++ index.js
@@ -1,9 +1,71 @@
 const button = document.getElementById('button');
 const content = document.getElementById('content');
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+const greetings = [
+    'Hello, how are you?',
+    'Welcome, I am Jars. Just a regular speech recognition',
+    'Hello there'
+]
+
+const forbidden = [
+    'Sorry but that word is prohibited to me',
+    'No curse words allowed',
+    'Hmm!'
+];
+
+const badSide = [
+    "You really don't wanna see my bad side you ugly piece of shit. Oops",
+    "Fuck that shit",
+    "Ooh fuck, here we go again"
+];
+
+const creator = [
+    "I was created by a brilliant programmer called Daniel",
+    "I am still under development, but my creator is Daniel"
+];
+
+const intro = [
+    "I am JARS which stands for 'Just a regular speech recognition"
+]
+
+recognition.onstart = () => {
+    console.log('Voice is activated')
+};
+
+recognition.onresult = (e) => {
+    const current = e.resultIndex;
+    const transcript = e.results[current][0].transcript;
+    content.innerHTML = transcript;
+    reply(transcript);
+    button.className = 'btn btn-primary shadow-none'
+}
 
 button.addEventListener('click', () => {
-    window.speechSynthesis.cancel();
-    const text = 'This is a test';
-    const utterance = new SpeechSynthesisUtterance(text);
-    window.speechSynthesis.speak(utterance)
-})
+    recognition.start()
+});
+
+function reply (message) {
+
+    if (message.includes('hello') || message.includes('hi')) {
+        const welcome = greetings[Math.floor(Math.random() * greetings.length)];
+        window.speechSynthesis.cancel();
+        const utterance = new SpeechSynthesisUtterance(welcome);
+        utterance.pitch = 1;
+        utterance.volume = 1.5
+        window.speechSynthesis.speak(utterance);
+    };
+    if (message.includes(['fuck', 'shit', 'bitch', 'idiot', 'bastard'])) {
+        const forbiddenReply = forbidden[Math.floor(Math.random() * forbidden.length)];
+        speech.text = forbiddenReply;
+    }
+    if (message.includes('Bad side') || message.include('your other side')) {
+        const badSideReply = badSide[Math.floor(Math.random() * badSide.length)];
+        speech.text = badSideReply;
+    }
+    if (message.includes(['who created you', 'created you', 'creator'])) {
+        const creatorReply = creator[Math.floor(Math.random() * creator.length)];
+        speech.text = creatorReply;
+    }
+}
