afeb751f54096e5cb845c8062b84d490d7c07c05
Daniel Toba <danieloloruntoba681@gmail.com>
2021-05-22 16:21:53 +0100

    Possible final change

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const greetings = [
     'Hello there'
 ]
 
+const answer =  [
+    'I am doing fine. The internet is a whole big world and I am glad to be part of it',
+    'I am okay today',
+    'A little bit okay'
+]
+
 const forbidden = [
     'Sorry but that word is prohibited to me',
     'No curse words allowed',
@@ -27,7 +33,7 @@ const creator = [
 ];
 
 const intro = [
-    "I am JARS which stands for 'Just a regular speech recognition"
+    "I am JARS which stands for 'Just a regular speech recognition'"
 ]
 
 recognition.onstart = () => {
@@ -58,14 +64,41 @@ function reply (message) {
     };
     if (message.includes(['fuck', 'shit', 'bitch', 'idiot', 'bastard'])) {
         const forbiddenReply = forbidden[Math.floor(Math.random() * forbidden.length)];
-        speech.text = forbiddenReply;
+        window.speechSynthesis.cancel();
+        const utterance = new SpeechSynthesisUtterance(forbiddenReply);
+        utterance.pitch = 1;
+        utterance.volume = 1.5
+        window.speechSynthesis.speak(utterance);
     }
     if (message.includes('Bad side') || message.include('your other side')) {
         const badSideReply = badSide[Math.floor(Math.random() * badSide.length)];
-        speech.text = badSideReply;
+        window.speechSynthesis.cancel();
+        const utterance = new SpeechSynthesisUtterance(badSideReply);
+        utterance.pitch = 1;
+        utterance.volume = 1.5
+        window.speechSynthesis.speak(utterance);
     }
-    if (message.includes(['who created you', 'created you', 'creator'])) {
+    if (message.includes('who created you') || message.includes('created you') || message.includes('creator')) {
         const creatorReply = creator[Math.floor(Math.random() * creator.length)];
-        speech.text = creatorReply;
+        window.speechSynthesis.cancel();
+        const utterance = new SpeechSynthesisUtterance(creatorReply);
+        utterance.pitch = 1;
+        utterance.volume = 1.5
+        window.speechSynthesis.speak(utterance);
+    }
+    if (message.includes('how are you') || message.includes('how you') || message.includes('you doing')) {
+        const feeling = answer[Math.floor(Math.random() * answer.length)];
+        window.speechSynthesis.cancel();
+        const utterance = new SpeechSynthesisUtterance(feeling);
+        utterance.pitch = 1;
+        utterance.volume = 1.5
+        window.speechSynthesis.speak(utterance);
+    } else {
+        window.speechSynthesis.cancel();
+        const speech = 'I do not understand what you mean yet'
+        const utterance = new SpeechSynthesisUtterance(speech);
+        utterance.pitch = 1.5;
+        utterance.volume = 1.5
+        window.speechSynthesis.speak(utterance);
     }
 }
